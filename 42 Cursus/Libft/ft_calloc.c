3a2b48cc3a9da4fbd17d65e436b3a842b64f3b88
Disaster-Loki <sebastiaol.carvalho@gmail.com>
2024-05-17 15:14:53 +0100

    version 2.6

---
+++ 42 Cursus/Libft/ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sde-carv <sde-carv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 14:49:19 by sde-carv          #+#    #+#             */
+/*   Updated: 2024/05/17 15:14:28 by sde-carv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size);
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*new;
+
+	new = malloc(nmemb * size);
+	if (!new)
+		return (0);
+	ft_bzero(new, nmemb * size);
+	return (new);
+}
