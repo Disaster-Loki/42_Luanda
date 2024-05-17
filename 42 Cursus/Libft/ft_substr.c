28f70e558886774862485e758311d4d5356063bd
Disaster-Loki <sebastiaol.carvalho@gmail.com>
2024-05-17 18:38:36 +0100

    version 2.7

--- 42 Cursus/Libft/ft_substr.c
+++ 42 Cursus/Libft/ft_substr.c
@@ -6,10 +6,25 @@
 /*   By: sde-carv <sde-carv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 17:54:03 by sde-carv          #+#    #+#             */
-/*   Updated: 2024/05/16 18:28:45 by sde-carv         ###   ########.fr       */
+/*   Updated: 2024/05/17 17:46:17 by sde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len){}
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*new;
+
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < start)
+		len = 0;
+	if (ft_strlen(s + start) < len)
+		len = ft_strlen(s + start);
+	new = malloc(sizeof(char) * (len + 1));
+	if (!new)
+		return (NULL);
+	ft_strlcpy(new, s + start, len + 1);
+	return (new);
+}
