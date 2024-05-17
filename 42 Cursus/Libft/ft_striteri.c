e28f166c1bee1271e90fe77a13aac38e33c900dc
Disaster-Loki <sebastiaol.carvalho@gmail.com>
2024-05-17 17:00:01 +0100

    version 2.6

--- 42 Cursus/Libft/ft_striteri.c
+++ 42 Cursus/Libft/ft_striteri.c
@@ -3,11 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sde-carv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: sde-carv <sde-carv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 17:59:18 by sde-carv          #+#    #+#             */
-/*   Updated: 2024/05/16 18:00:15 by sde-carv         ###   ########.fr       */
+/*   Updated: 2024/05/17 16:44:05 by sde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char*)){}
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	i;
+
+	if (!s)
+		return ;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
