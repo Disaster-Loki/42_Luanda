9b09f5ef032061b51fe41c7084c094d7e590d52a
Disaster-Loki <sebastiaol.carvalho22@gmail.com>
2024-05-19 17:23:39 +0100

    Version 2.9

--- 42 Cursus/Printf/ft_printf.c
+++ 42 Cursus/Printf/ft_printf.c
@@ -6,57 +6,56 @@
 /*   By: sde-carv <sde-carv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 13:18:05 by sde-carv          #+#    #+#             */
-/*   Updated: 2024/05/19 16:07:43 by sde-carv         ###   ########.fr       */
+/*   Updated: 2024/05/19 16:25:10 by sde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-static int	check_type(const char *input, void *arg)
+static int check_type(const char *input, va_list args)
 {
-	int	i;
+    int i = 0;
 
-	i = 0;
-	if (*input == 'c')
-		i += ft_print_char((int)arg);
-	else if (*input == 's')
-		i += ft_print_string((char *)arg);
-	else if (*input == 'p')
-		i += ft_print_pointer((unsigned long)arg, 87);
-	else if (*input == 'd')
-		i += ft_print_int((int)arg);
-	else if (*input == 'i')
-		i += ft_print_int((int)arg);
-	else if (*input == 'u')
-		i += ft_print_unsigned((unsigned int)arg);
-	else if (*input == 'x')
-		i += ft_print_hex((unsigned int)arg, 87);
-	else if (*input == 'X')
-		i += ft_print_hex((unsigned int)arg, 55);
-	return (i);
+    if (*input == 'c')
+        i += ft_print_char(va_arg(args, int));
+    else if (*input == 's')
+        i += ft_print_string(va_arg(args, char *));
+    else if (*input == 'p')
+        i += ft_print_pointer(va_arg(args, unsigned long), 87);
+    else if (*input == 'd' || *input == 'i')
+        i += ft_print_int(va_arg(args, int));
+    else if (*input == 'u')
+        i += ft_print_unsigned(va_arg(args, unsigned int));
+    else if (*input == 'x')
+        i += ft_print_hex(va_arg(args, unsigned int), 87);
+    else if (*input == 'X')
+        i += ft_print_hex(va_arg(args, unsigned int), 55);
+
+    return i;
 }
 
-int	ft_printf(const char *input, ...)
+int ft_printf(const char *input, ...)
 {
-	va_list			args;
-	unsigned int	i;
+    va_list args;
+    unsigned int i = 0;
 
-	i = 0;
-	va_start(args, input);
-	while (*input != '\0')
-	{
-		if (*input == '%')
-		{
-			input++;
-			if (ft_strchr("cspdiuxX", *input))
-				i += check_type(input, va_arg(args, void *));
-			else if (*input == '%')
-				i += ft_print_char('%');
-		}
-		else
-			i = i + ft_print_char(*input);
-		input++;
-	}
-	va_end(args);
-	return (i);
+    va_start(args, input);
+    while (*input != '\0')
+    {
+        if (*input == '%')
+        {
+            input++;
+            if (ft_strchr("cspdiuxX", *input))
+                i += check_type(input, args);
+            else if (*input == '%')
+                i += ft_print_char('%');
+        }
+        else
+        {
+            i += ft_print_char(*input);
+        }
+        input++;
+    }
+    va_end(args);
+    return i;
 }
--- 42 Cursus/Printf/header.h
+++ 42 Cursus/Printf/header.h
@@ -6,7 +6,7 @@
 /*   By: sde-carv <sde-carv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 13:07:59 by sde-carv          #+#    #+#             */
-/*   Updated: 2024/05/19 14:29:04 by sde-carv         ###   ########.fr       */
+/*   Updated: 2024/05/19 16:23:07 by sde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,13 @@
 # include <unistd.h>
 # include "./Libft/libft.h"
 
-int	ft_printf(const char *str, ...);
+int     ft_print_char(int c);
+int     ft_print_string(char *str);
+int     ft_print_pointer(unsigned long ptr, int base);
+int     ft_print_int(int n);
+int     ft_print_unsigned(unsigned int n);
+int     ft_print_hex(unsigned int n, int base);
+char    *ft_strchr(const char *s, int c);
+int	    ft_printf(const char *str, ...);
 
 #endif
