04132b9d08c9194380df342898740d545d709d75
Disaster-Loki <sebastiaol.carvalho22@gmail.com>
2024-05-24 16:01:48 +0100

    Version 3.2

--- 42 Cursus/Get_Next_Line/get_next_line.h
+++ 42 Cursus/Get_Next_Line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: sde-carv <sde-carv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 13:58:19 by sde-carv          #+#    #+#             */
-/*   Updated: 2024/05/23 16:16:57 by sde-carv         ###   ########.fr       */
+/*   Updated: 2024/05/24 14:41:26 by sde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,12 @@
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
+char	*ft_read(int fd, char *str);
+char	*ft_getrest(char *full_string);
+char	*ft_getline(char *full_string);
 char	*ft_strchr(const char *s, int c);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strjoin(const char *s1, const char *s2);
+int		main(void);
 
 #endif
