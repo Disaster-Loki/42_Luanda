a7d9686467af652809832cc340d97e253af2c2a8
Disaster-Loki <sebastiaol.carvalho22@gmail.com>
2024-06-14 16:06:13 +0100

    Version 3.5

--- 42 Cursus/Minitalk/server.c
+++ 42 Cursus/Minitalk/server.c
@@ -1,59 +1,52 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sde-carv <sde-carv@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/10 17:04:08 by sde-carv          #+#    #+#             */
-/*   Updated: 2024/06/14 12:31:29 by sde-carv         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minitalk.h"
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-static int				r_bit;
+static int r_bit;
 
-void	rest(int signal, siginfo *siginfo)
+void rest(int signal, siginfo_t *siginfo, void *context)
 {
-	static unsigned char	buff;
+    static unsigned char buff;
 
-	r_bit = 0
-	if (signal == SIGUSR1)
-		buff |= 1;
-	r_bit++;
-	if (r_bit == 8)
-	{
-		write(1, &buff, 1);
-		r_bit = 0;
-		buff = 0;
-	}
-	else
-		buff = buff << 1;
-	kill(siginfo->si_pid, SIGUSR1);
+    r_bit = 0;
+    (void)context;
+    if (signal == SIGUSR1)
+        buff |= 1;
+    r_bit++;
+    if (r_bit == 8)
+    {
+        write(1, &buff, 1);
+        r_bit = 0;
+        buff = 0;
+    }
+    else
+        buff = buff << 1;
+    kill(siginfo->si_pid, SIGUSR1);
 }
 
-void	sig_handler(void)
+void sig_handler(void)
 {
-	struct sigaction	sa;
+    struct sigaction sa;
 
-	sa.sa_sigaction = &rest;
-	sa.sa_flags = SA_SIGINFO;
-	sigemptyset(&sa.sa_mask);
-	if (sigaction(SIGUSR1, &sa, NULL) == -1)
-		(ft_printf("Error - Problem receiving signal"));
-	if (sigaction(SIGUSR2, &sa, NULL) == -1)
-		(ft_printf("Error - Problem receiving signal"));
+    sa.sa_sigaction = rest;
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(SIGUSR1, &sa, NULL) == -1)
+        perror("Error - Problem receiving SIGUSR1");
+    if (sigaction(SIGUSR2, &sa, NULL) == -1)
+        perror("Error - Problem receiving SIGUSR2");
 }
 
-int	main(void)
+int main(void)
 {
-	pid_t	pid;
+    pid_t pid;
 
-	pid = getpid();
-	ft_printf("Server PID: %i\n", pid);
-	sig_handler();
-	while (1)
-		pause();
-	return (0);
+    pid = getpid();
+    printf("Server PID: %i\n", pid);
+    sig_handler();
+    while (1)
+        pause();
+    return 0;
 }
